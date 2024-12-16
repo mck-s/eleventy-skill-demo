5ad5b94d07e52b40420200b3b4041d5b28435645
mck-s <mck-s@outlook.jp>
2024-12-17 00:04:59 +0900

    no message

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
     // Passthrough copy for CSS and images
     eleventyConfig.addPassthroughCopy('src/css/main.css');
     eleventyConfig.addPassthroughCopy('src/images');
+    eleventyConfig.addPassthroughCopy('src/js');
   
     // Return configuration for Eleventy
     return {
